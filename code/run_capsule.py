96f942b792d5130100e37ec55a1ff32973c388ed
houhan@gmail.com <han.hou@alleninstitute.org>
2024-09-04 02:38:42 +0000

    feat: fit one example session

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,38 @@
 """ top level run script """
 
+import multiprocessing as mp
+import numpy as np
+
+from nwb_io import get_history_from_nwb
+from aind_dynamic_foraging_models.generative_model import ForagerCollection
+
 def run():
-    """ basic run function """
-    pass
+    # Load data
+    session_id = "705599_2024-05-31_14-06-54"
+    (
+        baiting,
+        choice_history,
+        reward_history,
+        p_reward,
+        autowater_offered,
+        random_number,
+    ) = get_history_from_nwb(f"/root/capsule/data/foraging_nwb_bonsai/{session_id}.nwb")
+
+    # Remove NaNs
+    # TODO: handle in model fitting
+    
+    
+    # Initialize model
+    forager = ForagerCollection().get_preset_forager("Hattori2019", seed=42)
+    forager.fit(
+        choice_history,
+        reward_history,
+        DE_kwargs=dict(workers=1, disp=False, seed=np.random.default_rng(42)),
+        k_fold_cross_validation=0,
+    )
+    fig_fitting, axes = forager.plot_fitted_session(if_plot_latent=True)
+    
+    fig_fitting.savefig(f"results/{session_id}_Hattori_fitted.png")
+    
 
 if __name__ == "__main__": run()
