0fe7aa9dbe4905340c8c8daede2f03fbe7144689
houhan@gmail.com <han.hou@alleninstitute.org>
2024-09-17 22:29:41 +0000

    feat: also save json locally (ending up with dataasset)

--- code/utils/docDB_io.py
+++ code/utils/docDB_io.py
@@ -1,9 +1,11 @@
+import json
 from aind_data_access_api.document_db_ssh import DocumentDbSSHClient, DocumentDbSSHCredentials
 
 credentials = DocumentDbSSHCredentials()
 credentials.database = "behavior_analysis"
+local_results_root = "/root/capsule/results"
 
-def insert_docDB_ssh(result_dict, collection_name) -> int:
+def insert_docDB_ssh(result_dict, collection_name, if_save_local=True) -> int:
     """_summary_
 
     Parameters
@@ -12,6 +14,8 @@ def insert_docDB_ssh(result_dict, collection_name) -> int:
         bson-compatible result dictionary to be inserted
     collection_name : _type_
         name of the collection to insert into (such as "mle_fitting")
+    if_save_local : _type_, Optional
+        whether to save the result locally, by default True
 
     Returns
     -------
@@ -19,6 +23,12 @@ def insert_docDB_ssh(result_dict, collection_name) -> int:
         inserted_id
     """
     credentials.collection = collection_name
+    
+    if if_save_local:
+        # Save json locally
+        result_json = json.dumps(result_dict, indent=4)
+        with open(f"{local_results_root}/{result_dict['job_hash']}/result.json", "wb") as f:
+            json.dump(result_json, f)
 
     with DocumentDbSSHClient(credentials=credentials) as doc_db_client:
         response = doc_db_client.collection.insert_one(result_dict)
