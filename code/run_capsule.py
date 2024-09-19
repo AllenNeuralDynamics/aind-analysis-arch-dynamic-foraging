95e3a7ec7745a1ab24b99539dfe24c1293313d9a
Han Hou <han.hou@alleninstitute.org>
2024-09-19 06:48:18 +0000

    debug

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -174,6 +174,9 @@ if __name__ == "__main__":
 
     # retrive the arguments
     parallel_on_jobs = bool(int(args.parallel_on_jobs or "0"))  # Default 0
-    debug_mode = args.parallel_on_jobs == ""
+
+    debug_mode = args.parallel_on_jobs is None
+    print(debug_mode)
+    exit()
 
     run(parallel_on_jobs=parallel_on_jobs, debug_mode=debug_mode)
