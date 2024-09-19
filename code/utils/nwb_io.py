944c1a0023b730f8a93476294d105e7399aaa7ad
houhan@gmail.com <han.hou@alleninstitute.org>
2024-09-19 02:53:04 +0000

    feat: load nwb from s3 to bypass passing nwbs through CO pipeline

--- code/utils/nwb_io.py
+++ code/utils/nwb_io.py
@@ -2,8 +2,39 @@
 
 import numpy as np
 from pynwb import NWBHDF5IO
+import os
 
-def get_nwb(session_id):
+from utils.aws_io import fs
+
+S3_NWB_ROOT = "aind-behavior-data/foraging_nwb_bonsai"
+
+def get_nwb_from_s3(session_id):
+    """Get NWB file from session_id.
+
+    Overwrite this function to get NWB file from other places.
+
+    Parameters
+    ----------
+    session_id : _type_
+        _description_
+    """
+    local_temp_path = f"/tmp/{session_id}.nwb"
+        
+    # Open the file from S3 using fsspec
+    with fs.open(f"{S3_NWB_ROOT}/{session_id}.nwb", "rb") as f:
+        # Write the content to a local temporary file
+        with open(local_temp_path, "wb") as local_file:
+            local_file.write(f.read())
+
+    # Use NWBHDF5IO to read the NWB file from the local path
+    io = NWBHDF5IO(local_temp_path, mode="r")
+    nwb = io.read()
+
+    # Remove the local file after reading it
+    os.remove(local_temp_path)
+    return nwb
+
+def get_nwb_from_attached_dataasset(session_id):
     """Get NWB file from session_id.
 
     Overwrite this function to get NWB file from other places.
